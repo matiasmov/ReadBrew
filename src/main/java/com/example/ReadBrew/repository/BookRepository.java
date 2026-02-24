bee3ee942dede9c39ebddee389ac9279412eabe7
Lucas Matias <lucasmatias2005@gmail.com>
2026-02-24 02:35:52 -0300

    ♻️ refactor: change in the order of imports

--- src/main/java/com/example/ReadBrew/repository/BookRepository.java
+++ src/main/java/com/example/ReadBrew/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.example.ReadBrew.repository;
 
-import com.example.ReadBrew.model.Book;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
+
+import com.example.ReadBrew.model.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
