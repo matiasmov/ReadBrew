153104b8822982cf6a4eee422510c7172a2bb8c6
Lucas Matias <lucasmatias2005@gmail.com>
2026-02-23 23:00:29 -0300

    first commit

---
+++ src/test/java/com/example/ReadBrew/ReadBrewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReadBrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadBrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
