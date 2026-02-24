448be531508e08a131a20b0316fea3e884e2fb42
Lucas Matias <lucasmatias2005@gmail.com>
2026-02-24 00:43:30 -0300

    ✨feat: adding manual search in the repository

--- src/main/java/com/example/ReadBrew/repository/UserRepository.java
+++ src/main/java/com/example/ReadBrew/repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.example.ReadBrew.repository;
 
-public @interface UserRepository {
+import com.example.ReadBrew.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository 
+public interface UserRepository extends JpaRepository<User, Long> { 
     
+
+    User findByEmail(String email); 
 }
