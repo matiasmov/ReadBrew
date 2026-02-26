3e285ee280b44bb7573871a7d79182e376e61c8b
Lucas Matias <lucasmatias2005@gmail.com>
2026-02-26 19:27:19 -0300

    ♻️ refactor: adjusting the imports

--- src/main/java/com/example/ReadBrew/repository/CoffeeRepository.java
+++ src/main/java/com/example/ReadBrew/repository/CoffeeRepository.java
@@ -1,10 +1,11 @@
 package com.example.ReadBrew.repository;
 
-import com.example.ReadBrew.model.Coffee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.ReadBrew.model.Coffee;
+
 @Repository
 public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
-    
+  
 }
