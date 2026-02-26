caccea850c8ed756700f0e473610745f6096b8d5
Lucas Matias <lucasmatias2005@gmail.com>
2026-02-26 01:54:57 -0300

    ✨feat: adding a JPA interface to the CRUD of the coffee class

---
+++ src/main/java/com/example/ReadBrew/repository/CoffeeRepository.java
@@ -0,0 +1,10 @@
+package com.example.ReadBrew.repository;
+
+import com.example.ReadBrew.model.Coffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
+    
+}
