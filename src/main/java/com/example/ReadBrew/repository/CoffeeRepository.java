cc0c43aeadcafc6c16ccdb069a617b3f41a7be6a
Lucas Matias <lucasmatias2005@gmail.com>
2026-02-28 23:49:02 -0300

    ✨feat: dealing with coffee research

--- src/main/java/com/example/ReadBrew/repository/CoffeeRepository.java
+++ src/main/java/com/example/ReadBrew/repository/CoffeeRepository.java
@@ -1,11 +1,13 @@
 package com.example.ReadBrew.repository;
 
+import java.util.Optional; 
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.ReadBrew.model.Coffee;
 
-@Repository
 public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
-  
+    
+   
+    Optional<Coffee> findByNameContainingIgnoreCase(String name);
 }
