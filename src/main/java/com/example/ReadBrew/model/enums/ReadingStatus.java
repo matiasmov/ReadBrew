31737f2ac54302c1f0deb4caac47eb0f3856c3d5
Lucas Matias <lucasmatias2005@gmail.com>
2026-02-28 23:48:15 -0300

    ✨feat: improving the reader code and addressing the Lombok error

--- src/main/java/com/example/ReadBrew/model/enums/ReadingStatus.java
+++ src/main/java/com/example/ReadBrew/model/enums/ReadingStatus.java
@@ -1,8 +1,8 @@
 package com.example.ReadBrew.model.enums;
 
 public enum ReadingStatus {
-    WANT_TO_READ,
-    READING,
+    TO_READ,    
+    READING,    
     READ,
-    ABANDONED
+    ABANDONED       
 }
