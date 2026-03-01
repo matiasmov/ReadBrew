1fff44c29e897edc240fb7bd0bed0fdb87e3a6c7
Lucas Matias <lucasmatias2005@gmail.com>
2026-03-01 19:48:23 -0300

    ♻️ refactor: changing the search to nickname

--- src/main/java/com/example/ReadBrew/repository/UserRepository.java
+++ src/main/java/com/example/ReadBrew/repository/UserRepository.java
@@ -1,12 +1,16 @@
 package com.example.ReadBrew.repository;
 
-import com.example.ReadBrew.model.User;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.example.ReadBrew.model.User;
 
 @Repository 
 public interface UserRepository extends JpaRepository<User, Long> { 
     
 
     User findByEmail(String email); 
+    List<User> findByNicknameContainingIgnoreCase(String nickname);
 }
