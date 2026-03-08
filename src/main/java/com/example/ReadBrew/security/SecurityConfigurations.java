932f0b691478059cf9e07d32caafaa6511bec3b6
Lucas Matias <lucasmatias2005@gmail.com>
2026-03-08 00:16:24 -0300

    ✨feat: allowing requests through the frontend

--- src/main/java/com/example/ReadBrew/security/SecurityConfigurations.java
+++ src/main/java/com/example/ReadBrew/security/SecurityConfigurations.java
@@ -1,10 +1,13 @@
 package com.example.ReadBrew.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -26,27 +32,29 @@ public class SecurityConfigurations {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-    return httpSecurity
-        .csrf(csrf -> csrf.disable())
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(authorize -> authorize
-            // PUBLIC 
-            .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
-            .requestMatchers("/api/v1/auth/**").permitAll()
-            .requestMatchers("/error").permitAll() // I'll change
-            .requestMatchers(HttpMethod.GET, "/api/v1/users/avatars").permitAll() 
-            .requestMatchers("/images/**").permitAll()
-            
-            // ADM 
-            .requestMatchers(HttpMethod.GET, "/api/v1/users").hasRole("ADMIN")
-            .requestMatchers(HttpMethod.POST, "/api/v1/coffees").hasRole("ADMIN")
-            
-            // OTHERS
-            .anyRequest().authenticated()
-        )
-        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-        .build();
-}
+        return httpSecurity
+          
+            .cors(Customizer.withDefaults()) 
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authorize -> authorize
+                // PUBLIC 
+                .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers("/error").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/users/avatars").permitAll() 
+                .requestMatchers("/images/**").permitAll()
+                
+                // ADM 
+                .requestMatchers(HttpMethod.GET, "/api/v1/users").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/api/v1/coffees").hasRole("ADMIN")
+                
+                // OTHERS
+                .anyRequest().authenticated()
+            )
+            .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+            .build();
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -55,12 +63,32 @@ public class SecurityConfigurations {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-    return (web) -> web.ignoring()
-        .requestMatchers("/avatars/**", "/static/**", "/resources/**");
-}
+        return (web) -> web.ignoring()
+            .requestMatchers("/avatars/**", "/static/**", "/resources/**");
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); 
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        
+   
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); 
+        
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        
+ 
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        return source;
     }
 }
