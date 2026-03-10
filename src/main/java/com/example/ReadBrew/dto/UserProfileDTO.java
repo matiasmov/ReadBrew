6ed69e622fa5254b4cde3cf050f3c5f6fe39a804
Lucas Matias <lucasmatias2005@gmail.com>
2026-03-09 23:56:33 -0300

    ✨feat: adding followers and following in the DTO to display in the /me endpoint

--- src/main/java/com/example/ReadBrew/dto/UserProfileDTO.java
+++ src/main/java/com/example/ReadBrew/dto/UserProfileDTO.java
@@ -1,8 +1,9 @@
 package com.example.ReadBrew.dto;
 
+import java.util.List;
+
 import lombok.Builder;
 import lombok.Data;
-import java.util.List;
 
 @Data
 @Builder
@@ -12,8 +13,8 @@ public class UserProfileDTO {
     private String avatarUrl;
     private int level;
     private int currentXp;
-    
     private UserStatsDTO stats;
-    
     private List<UnlockedBadgeDTO> badges;
+    private int followersCount;
+    private int followingCount;
 }
