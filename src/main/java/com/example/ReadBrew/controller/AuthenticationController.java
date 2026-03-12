cbea39f084740463ad4e85248bdf6b0f02a574ad
Lucas Matias <lucasmatias2005@gmail.com>
2026-03-11 23:57:28 -0300

    ✨feat: adding access blocking due to excessive attempts

--- src/main/java/com/example/ReadBrew/controller/AuthenticationController.java
+++ src/main/java/com/example/ReadBrew/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.example.ReadBrew.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.ReadBrew.dto.AuthenticationDTO;
 import com.example.ReadBrew.dto.RegisterDTO;
 import com.example.ReadBrew.model.User;
+import com.example.ReadBrew.repository.UserRepository;
 import com.example.ReadBrew.security.TokenService;
 import com.example.ReadBrew.service.UserService;
 
@@ -33,20 +37,57 @@ public class AuthenticationController {
     @Autowired
     private UserService userService; 
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody AuthenticationDTO data) {
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
+        
+        User user = userRepository.findByEmail(data.email());
 
-        var token = tokenService.generateToken((User) auth.getPrincipal());
+        // check if the blocking time has elapsed
+        if (user != null) {
+            userService.unlockWhenTimeExpired(user);
+        }
 
-        return ResponseEntity.ok(token);
+        try {
+            
+            var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+            var auth = this.authenticationManager.authenticate(usernamePassword);
+
+          // reset attempts
+            if (user != null) {
+                userService.resetFailedAttempts(user);
+            }
+
+            var token = tokenService.generateToken((User) auth.getPrincipal());
+            return ResponseEntity.ok(token);
+
+        } catch (LockedException e) {
+
+            // blocked
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body("Conta bloqueada por excesso de tentativas. Aguarde 15 minutos e tente novamente.");
+                    
+        } catch (BadCredentialsException e) {
+            
+            // wrong password
+            if (user != null) {
+                userService.increaseFailedAttempts(user); // ++ attempts
+            }
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Credenciais inválidas!");
+                    
+        } catch (Exception e) {
+            // Others erros
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Erro na autenticação.");
+        }
     }
 
     @PostMapping("/register")
     public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
         try {
-            
             userService.createUser(data);
             return ResponseEntity.ok().build();
         } catch (RuntimeException e) {
@@ -54,13 +95,10 @@ public class AuthenticationController {
         }
     }
 
-   
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-    
         FieldError error = ex.getBindingResult().getFieldError();
         String errorMessage = error != null ? error.getDefaultMessage() : "Dados inválidos";
-        
         return ResponseEntity.badRequest().body(errorMessage);
     }
 }
