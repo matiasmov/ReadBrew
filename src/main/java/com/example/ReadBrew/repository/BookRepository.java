6a96f2f420f025fc7ecd7552b428fe58c36a8ac6
Lucas Matias <lucasmatias2005@gmail.com>
2026-03-30 00:01:10 -0300

    ✨ feat: bringing only books that have pictures to frontend

--- src/main/java/com/example/ReadBrew/repository/BookRepository.java
+++ src/main/java/com/example/ReadBrew/repository/BookRepository.java
@@ -1,9 +1,13 @@
 package com.example.ReadBrew.repository;
 
+
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Pageable;
 
 import com.example.ReadBrew.model.Book;
 
@@ -12,4 +16,10 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByIsbn(String isbn);
 
     Optional<Book> findByExternalId(String externalId);
+
+    @Query("SELECT b FROM Book b WHERE b.title IS NOT NULL AND b.title <> '' " +
+            "AND b.thumbnailUrl IS NOT NULL AND b.thumbnailUrl <> '' " +
+            "AND b.authors IS NOT EMPTY")
+    Page<Book> findCompleteBooks(Pageable pageable);
+
 }
